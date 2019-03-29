53f366e0537d768e4c5ed540bc1609373eb6bf3e
Alex McCulloch <alexmccx@gmail.com>
2019-03-28 23:38:30 -0400

    Desklist Lookup

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 Make high quality Netrunner proxies. Generates ready to print PDFs from a NetrunnerDB deck list, a retail set, or a custom card list. 
 
 Based on this https://codepen.io/Theavon/pen/YQmgBG
+and this https://gparmer.github.io/anrproxy/
 
 Uses scans from /u/LepcisMagna/ https://www.reddit.com/r/Netrunner/comments/8pgfbj/android_netrunner_cards_up_to_kitara_600dpi/
 
@@ -12,13 +13,11 @@ Images de-speckled using https://gist.github.com/ankitjaininfo/86e7e07a8df12e246
 and slightly enhanced using mogrify -modulate 105,105
 
 
-
 ## Intended features
  
-* Option to generate MPC printable images, including border frame/bleed
+* Option to generate MPC printable images, including black border frame/bleed
 * Under retail sets, allow picking 1 set vs play set for Core/revised core/SC19
 * Under card list, allow alt art selection
-
 
 
 Requires images directory, filled with images named after their NetrunnerDB id.
--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,7 @@
 var _cardDB           = {};
+var _cardDB_keyID     = {}; //same as _cardDB, but keyed by card code instead of name
 var _userInputElem    = $('#UserInput');
+var _deckID           = $('#DeckId');
 var _cardListElem     = $('#Cards');
 var _cardListHtml     = '';
 
@@ -16,21 +18,37 @@ function selectTab(evt, tabLabel) {
   }
   document.getElementById(tabLabel).style.display = "block";
   evt.currentTarget.className += " active";
+
+  console.log(tabLabel);
+  switch(tabLabel) {
+    case "Desklist":
+      buildFromDeckID();      
+      break;
+    case "Set":
+      break;
+    case "Card List":
+      buildFromCardList();
+      break;
+  }
 }
 
 function loadCards() {
-  _cardDB = localStorage.getItem('cards');
+  _cardDB = localStorage.getItem('cardsDB');
   _cardDB = JSON.parse(_cardDB);
+
+  _cardDB_keyID = localStorage.getItem('cardDB_keyID');
+  _cardDB_keyID = JSON.parse(_cardDB_keyID);
   
   if (!_cardDB) {
     fetchAllCards();
   } else {
-    buildList();
+    buildFromCardList();
   }
 }
 
 function saveCards() {
-  localStorage.setItem('cards', JSON.stringify(_cardDB));
+  localStorage.setItem('cardsDB', JSON.stringify(_cardDB));
+  localStorage.setItem('cardDB_keyID', JSON.stringify(_cardDB_keyID));
 }
 
 function reset() {
@@ -43,25 +61,29 @@ function fetchAllCards() {
 
   $.getJSON( "https://netrunnerdb.com/api/2.0/public/cards", function(response) {
     _cardDB = {};
+    _cardDB_keyID = {};
 
     $.each(response.data, function(key, item) {
         var image = 'https://proxynexus.z27.web.core.windows.net/images/' + item.code + '.jpg';
-
-        console.log(item);
 
         _cardDB[item.title.toLowerCase().replace(/:/g, '').replace(/\s/g, '__')] = {
           code: item.code,
           image: image
         }
+
+        _cardDB_keyID[item.code] = {
+          code: item.code,
+          title: item.title,
+          image: image
+        }
     });
     
-    // console.log('cards fetched from api');
     saveCards();
-    buildList();
+    buildFromCardList();
   });
 }
 
-function buildList() {
+function buildFromCardList() {
   var html = '';
   var input = _userInputElem.val().toLowerCase().split(/\n/);
   var unfound = 0;
@@ -80,13 +102,10 @@ function buildList() {
     if (cardname in _cardDB) {
       var card = _cardDB[cardname];
       var newCard = '';
-
       newCard += '<a href="https://netrunnerdb.com/en/card/' + card.code + '" title="" target="NetrunnerCard">';
       newCard += '<img class="card" src="' + card.image + '" alt="' + card.code + '" />';
       newCard += '<span class="label print-hide">' + card.code + ' ' + cardname.replace(/__/g, ' ') + '</span>'; 
       newCard += '</a>'; 
-
-      // console.log(JSON.stringify(card) + ' ' + cardname);
       html += newCard;
     } else {
       unfound++;
@@ -101,17 +120,58 @@ function buildList() {
     _cardListHtml = html;
     _cardListElem.html(_cardListHtml);
   } 
-} 
+}
+
+function buildFromDeckID() {
+  var html = '';
+  var deckInput = _deckID.val().toLowerCase();
+  var deckidregex = /\d\d\d\d\d/;
+  if (!_cardDB_keyID) {
+    return false;
+  }
+
+  var match = deckidregex.exec(deckInput);
+  if (match == null) return;
+
+  var deckid = match[0];
+  $.getJSON("https://netrunnerdb.com/api/2.0/public/decklist/" + deckid, function(response) {
+    var input = response.data[0].cards;
+    var keys = Object.keys(input);
+    for (var i = 0; i < keys.length; i++) {
+      if (keys[i] in _cardDB_keyID) {
+        var card = _cardDB_keyID[keys[i]];
+        for (var j = 0; j < input[keys[i]]; j++) {
+          var newCard = '';
+          newCard += '<a href="https://netrunnerdb.com/en/card/' + card.code + '" title="" target="NetrunnerCard">';
+          newCard += '<img class="card" src="' + card.image + '" alt="' + card.code + '" />';
+          newCard += '<span class="label print-hide">' + card.code + ' ' + card.title.replace(/__/g, ' ') + '</span>'; 
+          newCard += '</a>'; 
+          html += newCard;
+        }
+      }
+    }
+
+    if (_cardListHtml != html) {
+      _cardListHtml = html;
+      _cardListElem.html(_cardListHtml);
+    }
+  });
+  _deckID.val(deckid);
+}
 
 function assignEvents() {
-  $(document).on('input propertychange', _userInputElem, function() {
-    buildList();
-  });  
+  _userInputElem.on('input',function(e){
+    buildFromCardList();
+  });
+
+  _deckID.on('input',function(e){
+    buildFromDeckID();
+  });
 }
 
 $(function() {
   document.getElementById("defaultOpen").click();
-  _userInputElem.val("MKUltra\nParagon\nHayley Kaplan: Universal Scholar");
+  _userInputElem.text("MKUltra\nParagon\nHayley Kaplan: Universal Scholar");
   assignEvents();
   loadCards();
 });
