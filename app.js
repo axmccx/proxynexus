a71fbd0cc20d1f91614cf8b1d1fc971e16ceb0a0
Alex McCulloch <alexmccx@gmail.com>
2019-04-24 22:11:28 -0400

    Major change to image fetching Images downloaded to files instead buffers.
    Reduced memory usage.
    PDFkit doesn't appear to grow the pdf files,
    when duplicate images are added from files.
    Usually leads to reduces pdf size.

--- app.js
+++ app.js
@@ -1,49 +1,36 @@
 const port = process.env.PORT || 8000;
 var fs  = require('fs');
+const fetch = require("node-fetch");
 var express = require("express");
 var app = express();
 app.use(express.static('static'));
-
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+const PDFDocument = require('pdfkit');
 var crypto = require('crypto');
 
-const {
-	Aborter,
-	BlockBlobURL,
-	ContainerURL,
-	ServiceURL,
-	StorageURL,
-	SharedKeyCredential,
-	downloadBlobToBuffer
-  } = require("@azure/storage-blob");
-
-if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config();
-}
-
-const STORAGE_ACCOUNT_NAME = process.env.AZURE_STORAGE_ACCOUNT_NAME;
-const ACCOUNT_ACCESS_KEY = process.env.AZURE_STORAGE_ACCOUNT_ACCESS_KEY;
-const ONE_MINUTE = 60 * 1000;
-
-const PDFDocument = require('pdfkit');
+// if (process.env.NODE_ENV !== "production") {
+//     require("dotenv").config();
+// }
 
 function cmToPt (cm) {
 	return cm * 28.3465;
 }
-
 var IDCounter = 1;
-
 const cardwidth = 6.35;
 const cardheight = 8.80;
-
 const cardwidthPt = cmToPt(cardwidth);
 const cardheightPt = cmToPt(cardheight);
 
-app.post('/api/makePDF', function (req, res) {
+// setInterval(function() {
+// 	const used = process.memoryUsage();
+// 	for (let key in used) {
+// 		console.log(`${key} ${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`);
+// 	}
+// }, 100);
 
+app.post('/api/makePDF', function (req, res) {
 	const paperSize = req.body.paperSize;
 	const quality = req.body.quality;
 	const logInfo = req.body.logInfo;
@@ -70,13 +57,11 @@ app.post('/api/makePDF', function (req, res) {
 		return;
 	}
 
-	const request = paperSize + quality + requestedImages;
-	const hash = crypto.createHash('sha1').update(request).digest('hex');
+	const requestedPDFOptions = paperSize + quality + requestedImages;
+	const hash = crypto.createHash('sha1').update(requestedPDFOptions).digest('hex');
 	const pdfPath = __dirname + "/static/tmp/" + hash + ".pdf";
 
 	console.log(Date() + " DownloadID: " + downloadID + "; PDF Name: " + hash + ".pdf");
-
-	// TODO check for hard coded PDFs in storage here
 
 	if (fs.existsSync(pdfPath)) {
 		console.log(Date() + " DownloadID: " + downloadID + "; PDF already exists, no need to generate");
@@ -106,7 +91,7 @@ app.post('/api/makePDF', function (req, res) {
 		return;
 	}
 
-	const doc = new PDFDocument({
+	var doc = new PDFDocument({
 		size: paperSize,
 		margins: {
 			top: topMargin,
@@ -116,51 +101,71 @@ app.post('/api/makePDF', function (req, res) {
 		  }
 	});
 
-	fetchImages(requestedImages, container, downloadID)
-		.then(imgBufferList => {
-			console.log(Date() + " DownloadID: " + downloadID + "; Adding images to doc...");
-
-			doc.pipe(fs.createWriteStream(pdfPath));
-			makeFrontPage(doc);
-			doc.addPage();
-			drawCutLines(doc, leftMargin, topMargin);
-			addImages(imgBufferList, doc, leftMargin, topMargin);
-			doc.end();
-
-			// does nulling the buffer list help free memory?
-			imgBufferList = null; 
-
-			res.status(200);
-			var result = {}
-			result.success = true;
-			result.fileName = "/tmp/" + hash + ".pdf";
-			res.json(result);
-
-			console.log(Date() + " DownloadID: " + downloadID + "; Download link sent to client");
-			return;
-		})
-		.catch(err => {
-			doc.end();
-			console.error("Error fetching images");
-			console.error(err);
-			res.status(200);
-			var result = {}
-			result.success = false;
-			result.errorMsg = "Error fetching images, try again";
-			res.json(result);
-			return;
-		})
+	fetchImages(requestedImages, doc, container, pdfPath, downloadID, topMargin, leftMargin, res, hash);
 });
+
+async function fetchImages(requestedImages, doc, container, pdfPath, downloadID, topMargin, leftMargin, res, hash) {	
+
+	var imgCodeList = [...requestedImages];
+	const biotechIndex = imgCodeList.indexOf("08012");
+	if (biotechIndex >= 0) {
+		const extraCodes = ["08012a", "08012", "08012b", "08012", "08012c"];
+		imgCodeList.splice(biotechIndex + 1, 0, ...extraCodes);
+	}
+	const syncIndex = imgCodeList.indexOf("09001");
+	if (syncIndex >= 0) {
+		imgCodeList.splice(syncIndex + 1, 0, "09001a");
+	}
+
+	console.log(Date() + " DownloadID: " + downloadID + "; Code list ready, Fetching images...");
+
+	const imgPromises = imgCodeList.map( async code => {
+		const imgPath = "./static/tmp/" + code + ".jpg";
+		const url = "https://proxynexus.blob.core.windows.net/" + container + "/" + code + ".jpg";
+		const res = await fetch(url);
+		const fileStream = fs.createWriteStream(imgPath);
+		return new Promise((resolve, reject) => {
+			res.body.pipe(fileStream);
+			res.body.on("error", (err) => {
+				reject(err);
+			});
+			fileStream.on("finish", function() {
+				resolve();
+			});
+		});
+	});
+
+	await Promise.all(imgPromises);
+
+	console.log(Date() + " DownloadID: " + downloadID + "; Adding images to doc...");
+
+	doc.pipe(fs.createWriteStream(pdfPath));
+	makeFrontPage(doc);
+	doc.addPage();
+	drawCutLines(doc, leftMargin, topMargin);
+	addImages(requestedImages, doc, leftMargin, topMargin);
+	doc.end();
+
+	res.status(200);
+	var result = {}
+	result.success = true;
+	result.fileName = "/tmp/" + hash + ".pdf";
+	res.json(result);
+
+	console.log(Date() + " DownloadID: " + downloadID + "; Download link sent to client");
+	return;
+}
 
 function addImages(lst, doc, leftMargin, topMargin) {
 	var rowCount = 0;
 	var colCount = 0;
 
-	lst.forEach((buffer, i) => {
+	lst.forEach((code, i) => {
 		const x = rowCount*cardwidthPt + leftMargin;
 		const y = colCount*cardheightPt + topMargin;
+		const imgPath = "static/tmp/" + code + ".jpg";
 
-		doc.image(buffer, x, y, {width: cardwidthPt, height: cardheightPt});
+		doc.image(imgPath, x, y, {width: cardwidthPt, height: cardheightPt});
 		rowCount++;
 
 		if (rowCount > 2) {
@@ -176,57 +181,6 @@ function addImages(lst, doc, leftMargin, topMargin) {
 	return;
 }
 
-async function fetchImages(requestedImages, container, downloadID) {	
-	const credentials = new SharedKeyCredential(STORAGE_ACCOUNT_NAME, ACCOUNT_ACCESS_KEY);
-  const pipeline = StorageURL.newPipeline(credentials);
-	const serviceURL = new ServiceURL(`https://${STORAGE_ACCOUNT_NAME}.blob.core.windows.net`, pipeline);
-	const containerURL = ContainerURL.fromServiceURL(serviceURL, container);
-
-	var imgCodeList = [...requestedImages];
-	const biotechIndex = imgCodeList.indexOf("08012");
-	if (biotechIndex >= 0) {
-		const extraCodes = ["08012a", "08012", "08012b", "08012", "08012c"];
-		imgCodeList.splice(biotechIndex + 1, 0, ...extraCodes);
-	}
-	const syncIndex = imgCodeList.indexOf("09001");
-	if (syncIndex >= 0) {
-		imgCodeList.splice(syncIndex + 1, 0, "09001a");
-	}
-
-	console.log(Date() + " DownloadID: " + downloadID + "; Code list ready, Fetching images...");
-
-	const imgPromiseList = imgCodeList.map(async code => {
-		const blobName = code + '.jpg';
-		const blockBlobURL = BlockBlobURL.fromContainerURL(containerURL, blobName);
-		var aborter = createAborter();
-		const downloadResponse = await blockBlobURL.download(aborter, 0);
-
-		const fileSize = downloadResponse.contentLength;
-		const buffer = Buffer.alloc(fileSize);
-		await downloadBlobToBuffer(
-			aborter,
-			buffer,
-			blockBlobURL,
-			0,
-			undefined,
-			{
-				blockSize: 4 * 1024 * 1024, // 4MB block size
-				parallelism: 20, // 20 concurrency
-				progress: ev => console.log("DocumentID: " + downloadID + ", LoadedBytes: " + ev.loadedBytes)
-			}
-		);
-		aborter.cancelTimer();
-		return buffer;
-	});
-
-	const imgBufferList = await Promise.all(imgPromiseList);
-	return imgBufferList;
-}
-
-function createAborter() {
-	return Aborter.timeout(30 * ONE_MINUTE);
-}
-
 function drawCutLines(doc, leftMargin, topMargin) {
 	doc.lineWidth(0.5);
 
@@ -236,7 +190,6 @@ function drawCutLines(doc, leftMargin, topMargin) {
 	doc.moveTo(x, y)
 	   .lineTo(x, y-10)
 	   .stroke();
-
 	x += cardwidthPt;
 	doc.moveTo(x, y)
 	   .lineTo(x, y-10)
@@ -248,7 +201,6 @@ function drawCutLines(doc, leftMargin, topMargin) {
 	doc.moveTo(x, y)
 	   .lineTo(x-18, y)
 	   .stroke();
-
 	x += 3*cardwidthPt;
 	doc.moveTo(x, y)
 	   .lineTo(x+18, y)
@@ -260,7 +212,6 @@ function drawCutLines(doc, leftMargin, topMargin) {
 	doc.moveTo(x, y)
 	   .lineTo(x-18, y)
 	   .stroke();
-
 	x += 3*cardwidthPt;
 	doc.moveTo(x, y)
 	   .lineTo(x+18, y)
@@ -272,11 +223,11 @@ function drawCutLines(doc, leftMargin, topMargin) {
 	doc.moveTo(x, y)
 	   .lineTo(x, y+10)
 	   .stroke();
-
 	x += cardwidthPt;
 	doc.moveTo(x, y)
 	   .lineTo(x, y+10)
-	   .stroke();
+		 .stroke();
+	return;
 }
 
 function makeFrontPage(doc) {
@@ -291,6 +242,7 @@ function makeFrontPage(doc) {
 	doc.text('Print this PDF at 100% size with no additional margins.', {
 		align: 'center'
 	});
+	return;
 }
 
 app.listen(port, () => {
