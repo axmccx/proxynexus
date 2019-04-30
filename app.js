b7576b216802248e18225ee14907fac0d5b46883
Alex McCulloch <alexmccx@gmail.com>
2019-04-30 09:49:56 -0400

    add readme file to zip

--- app.js
+++ app.js
@@ -627,6 +627,7 @@ async function fetchImagesForZip(opt) {
 	cardBacks.forEach(file => {
 		archive.file(__dirname + "/static/tmp/zip-cache/" + file, { name: file });
 	});
+	archive.file(__dirname + "/static/tmp/zip-cache/README.txt", { name: "README.txt" });
 	archive.finalize();
 }
 
