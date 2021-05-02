cc16e5e745a3e0e2c09512b089588d5e63571f06
Alex McCulloch <alexmccx@gmail.com>
2021-05-02 10:58:42 -0400

    Add generators placeholders

---
+++ generators/index.js
@@ -0,0 +1,7 @@
+export function generatePdf(cardList, includeCardBacks, PdfPageSize, fullCutLines) {
+  console.log(cardList, includeCardBacks, PdfPageSize, fullCutLines);
+}
+
+export function generateMpc(cardList, includeCardBacks, LmMpcPlacement) {
+  console.log(cardList, includeCardBacks, LmMpcPlacement);
+}
