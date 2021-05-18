0f5c8b911e66969d215f46940b073ecc4eb25923
Alex McCulloch <alexmccx@gmail.com>
2021-05-18 15:51:20 -0400

    Add some of the new image files, update scripts

--- misc/make_cardfile_json.py
+++ misc/make_cardfile_json.py
@@ -8,18 +8,27 @@ files_list = [f"{path}{f}" for f in listdir(path) if isfile(join(path, f))]
 store = []
 
 count = 0
+found_lm_codes = []
+found_pt_codes = []
+found_de_codes = []
+
 
 for f in files_list:
-    if 'pdf' not in f:
-        continue
-
-    count += 1
     base = basename(f)
-
+    if base == 'runner_back.png' or base == 'corp_back.png':
+        continue
     split_f = base.split('_')
     code = split_f[0]
     source = split_f[1]
+    if source == 'lm' and code not in found_lm_codes:
+        found_lm_codes.append(code)
+    elif source == 'pt' and code not in found_pt_codes:
+        found_pt_codes.append(code)
+    elif source == 'de' and code not in found_de_codes:
+        found_de_codes.append(code)
 
+for code in found_lm_codes:
+    count += 1
     new_entry = {
         "pdf": "",
         "pdf_back": "",
@@ -30,11 +39,52 @@ for f in files_list:
         "preview": "",
         "preview_back": ""
     }
-
     suffixes = ["pdf", "pdf_back", "fitted", "fitted_back", "scaled", "scaled_back", "prev", "prev_back"]
     for key, suffix in zip(new_entry.keys(), suffixes):
-        if f"{path}{code}_{source}_{suffix}.jpg" in files_list:
-            new_entry[key] = f"{code}_{source}_{suffix}.jpg"
+        if f"{path}{code}_lm_{suffix}.jpg" in files_list:
+            new_entry[key] = f"{code}_lm_{suffix}.jpg"
+
+    new_entry["id"] = count
+    store.append(new_entry)
+
+
+for code in found_pt_codes:
+    count += 1
+    new_entry = {
+        "pdf": "",
+        "pdf_back": "",
+        "mpc_fitted": "",
+        "mpc_fitted_back": "",
+        "preview": "",
+        "preview_back": ""
+    }
+    suffixes = ["pdf", "pdf_back", "mpc", "mpc_back", "prev", "prev_back"]
+    for key, suffix in zip(new_entry.keys(), suffixes):
+        if f"{path}{code}_pt_{suffix}.png" in files_list:
+            new_entry[key] = f"{code}_pt_{suffix}.png"
+
+    new_entry["mpc_scaled"] = ""
+    new_entry["mpc_scaled_back"] = ""
+    new_entry["id"] = count
+    store.append(new_entry)
+
+
+for code in found_de_codes:
+    count += 1
+    new_entry = {
+        "pdf": "",
+        "pdf_back": "",
+        "mpc_fitted": "",
+        "mpc_fitted_back": "",
+        "mpc_scaled": "",
+        "mpc_scaled_back": "",
+        "preview": "",
+        "preview_back": ""
+    }
+    suffixes = ["pdf", "pdf_back", "fitted", "fitted_back", "scaled", "scaled_back", "prev", "prev_back"]
+    for key, suffix in zip(new_entry.keys(), suffixes):
+        if f"{path}{code}_de_{suffix}.jpg" in files_list:
+            new_entry[key] = f"{code}_de_{suffix}.jpg"
 
     new_entry["id"] = count
     store.append(new_entry)
@@ -42,4 +92,3 @@ for f in files_list:
 
 with open('cardfiles.json', 'w') as fp:
     json.dump(store, fp)
-
---
+++ misc/mass_resize.py
@@ -0,0 +1,34 @@
+import os
+import cv2
+
+
+def load_images(path):
+    if os.path.isfile(path):
+        return [cv2.imread(path, cv2.IMREAD_COLOR)], [os.path.basename(path)]
+    elif os.path.isdir(path):
+        images, filenames = [], []
+        for filename in os.listdir(path):
+            if "_pdf" in filename:
+                img = cv2.imread(os.path.join(path, filename))
+                if img is not None:
+                    images.append(img)
+                    filenames.append(filename)
+        return images, filenames
+    else:
+        raise ValueError("Provided path is neither a file or directory")
+
+def mass_resize(path):
+    imgs, filenames = load_images(path)
+    scale_percent = 25
+
+    for img, filename in zip(imgs, filenames):
+        width = int(img.shape[1] * scale_percent / 100)
+        height = int(img.shape[0] * scale_percent / 100)
+        dim = (width, height)
+        prev_img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+        filename, extension = os.path.splitext(filename)
+        code = filename.split('_')[0]
+        cv2.imwrite(f"{path}/{code}_pt_prev{extension}", prev_img)
+
+
+mass_resize("C:\\Users\\alexm\\Downloads\\Netrunner\\version2")
